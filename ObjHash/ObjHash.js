48bc1973684213140a10adc619f7404eefce9f88
buzzwashere <buzz.loftus@gmail.com>
2011-04-01 03:27:28 +0000

    minor tweaking

--- ObjHash/ObjHash.js
+++ ObjHash/ObjHash.js
@@ -1,3 +1,36 @@
+/* ----------------------------------------------------------------------------------
+
+ * ObjHash.js 
+ *
+ *  This utility provides a simple way to create and maintain an array, or hash,
+ *  of objects in javascript.  
+ *
+ *  To create a new object hash:
+ *
+ *      var myHash = new util.ObjHash();
+ *      myHash.initialize();
+ *
+ *  To add an object to the hash:
+ *
+ *      myHash.addObject({
+ *          name: 'object 1',
+ *		  attr1: 'this is attr1 of object 1',
+ *		  attr2: 'this is attr2 of object 1'
+ *      });
+ *
+ *  To retrieve an object from the hash:
+ *
+ *      var myObject = myHash.getObject('name','object 1');
+ *
+ *  To update an attribute of an object on the hash:
+ *
+ *      myHash.updateAttribute('name', 'object 1', 'attr1', 'whats_up_dog?');
+ *
+ *  To remove an object from the hash:
+ *
+ *      var myObject = myHash.removeObject('name','object 1');
+ *      
+ * ---------------------------------------------------------------------------------- */
 
 var util = util ? util : {};
 util.ObjHash = util.ObjHash ? util.ObjHash : {};
@@ -7,12 +40,12 @@ util.ObjHash = function() {};
 util.ObjHash.prototype.initialize = function() {
 	
     var scope = this;
-	this.array = new Array();
+	this.array = [];
 	this.index = 0;
 
 	// Add an object to the array.
-	// 	Params:
-	//		newObject 		= the object to add to the array 
+	//  Params:
+	//		newObject = the object to add to the array 
 	//  Returns:
 	//		Boolean... true = object was added, false = not
 	this.addObject = function( newObject ) {
@@ -22,17 +55,17 @@ util.ObjHash.prototype.initialize = function() {
 		} catch(err) {
 			return false;
 		}
-	}
+    };
 
 	// Retrieve an object from the array.
-	// 	Params:
-	//		searchName 		= name of attribute by which to find object 
-	//		searchValue 	= value of attribute by which to find object 
+	//  Params:
+	//		searchName = name of attribute by which to find object 
+	//		searchValue = value of attribute by which to find object 
 	//  Returns:
 	//		the object found, or null if not found
 	this.getObject = function( searchName, searchValue ) {
 		try{
-			if (searchName != "" && searchValue != "") {
+			if (searchName !== "" && searchValue !== "") {
 				for (var i = 0; i < this.array.length; i++) {
 					if (this.array[i][searchName] == searchValue) {
 						this.index = i;
@@ -44,12 +77,12 @@ util.ObjHash.prototype.initialize = function() {
 		} catch(err) {
 			return null;
 		}
-	}
+	};
 	
 	// Remove an object from the array.
-	// 	Params:
-	//		searchName 		= name of attribute by which to find object 
-	//		searchValue 	= value of attribute by which to find object 
+	//  Params:
+	//		searchName = name of attribute by which to find object 
+	//		searchValue = value of attribute by which to find object 
 	//  Returns:
 	//		Boolean... true = object was found and removed, false = not
 	this.removeObject = function( searchName, searchValue ) {
@@ -65,13 +98,13 @@ util.ObjHash.prototype.initialize = function() {
 			return false;
 		}
 		
-	}
+	};
 	
 	// Update a the value of an nattribute within an object of the array.  
-	// 	Params:
-	//		searchName 		= name of attribute by which to find object 
-	//		searchValue 	= value of attribute by which to find object 
-	//		updateAttrName 	= name of attribute to update within object 
+	//  Params:
+	//		searchName = name of attribute by which to find object 
+	//		searchValue = value of attribute by which to find object 
+	//		updateAttrName = name of attribute to update within object 
 	//		updateAttrValue = value to assign to object attribute 
 	//  Returns:
 	//		Boolean... true = object was found and updated, false = not
@@ -87,6 +120,6 @@ util.ObjHash.prototype.initialize = function() {
 		} catch(err) {
 			return false;
 		}
-	}
+    };
 
 };
--- ObjHash/testobjhash.js
+++ ObjHash/testobjhash.js
@@ -4,32 +4,36 @@
 
 function testit() {
 
-	var myArray = new util.ObjHash();
-	myArray.initialize();
+    var results;
 
-	myArray.addObject(
+	var myHash = new util.ObjHash();
+	myHash.initialize();
+
+	myHash.addObject(
 		{
 			name: 'object 1',
 			attr1: 'this is attr1 of object 1',
 			attr2: 'this is attr2 of object 1'
 		}
 	);
-	myArray.addObject(
+    results = "1. An object was added to the hash with name: " + myHash.getObject('name','object 1').name;
+
+    myHash.addObject(
 		{
 			name: 'object 2',
 			attr1: 'this is attr1 of object 2',
 			attr2: 'this is attr2 of object 2'
 		}
 	);
+    results += "<br>2. An object was added to the hash with name: " + myHash.getObject('name','object 2').name;
 
-	var results;
-	var x = myArray.getObject('name','object 1');
-	if (x) {
-		results = "get object 1 = " + x.attr1;
-	} else {
-		results = "get object 1 failed";
-	}
-	
-	document.getElementById('content').innerHTML = results;
+    myHash.removeObject('name','object 1');
+    if (myHash.getObject('name','object 1') === null) {
+        results += "<br>3. 'object 1' was removed from the hash";
+    } else {
+        results += "<br>3. 'object 1' was NOT removed from the hash";
+    }        
+
+    document.getElementById('content').innerHTML = results;
 
 }
