59fed7a51553b397e43a779d5488de49acb85cd4
Buzz Loftus <buzz.loftus@gmail.com>
2013-03-27 09:50:32 -0300

    Create inheritance.js

---
+++ inheritance.js
@@ -0,0 +1,66 @@
+// --------------------------------------------------
+// simple inheritance
+
+// The function below returns 'this'.  New objects of 
+// type SimpleObject can be created using the 'new' operator.
+
+var SimpleObject = function() {
+    this.sayIt = function( text ) {
+        var theText = text != undefined && text != '' ? text : 'nothing';
+        console.log( 'the text is: ' + theText );
+    }
+    return this;  // allows SimpleObject to called directly (w/o newing up)
+}
+
+// create new SimpleObject.
+var newObj = new SimpleObject();
+newObj.sayIt('it worked');
+
+// create another new SimpleObject.
+var newObj2 = new SimpleObject();
+newObj2.sayIt('this one also worked');
+
+newObj.sayIt();
+newObj2.sayIt('and another thing');
+
+
+// --------------------------------------------------
+// Factory method
+
+// This function returns an object declared inside itself.
+// The returned object is always new because local function 
+// variables are thrown away when function execution ends,
+// except in the case of a closure.
+
+function factoryFunction(manuf, model) {
+    var obj = {
+        mfr: manuf,
+        mdl: model,
+		describe: function() {
+			console.log(this.mfr + ' ' + this.mdl);
+		}
+    };
+    // other code to manipulate obj in some way here
+    return obj;
+}
+
+var hondaCar = factoryFunction('Honda','Accord');
+hondaCar.describe();
+
+var mazdaCar = factoryFunction('Mazda','Miata');
+mazdaCar.describe();
+
+
+// --------------------------------------------------
+// simple object with two properties and one function
+
+var myobj = {
+    prop1: 'thing1',
+    prop2: 'thing2',
+    fn1: function() {
+        console.log(this.prop1 + ' ... ' + this.prop2);
+    }
+};
+
+myobj.fn1();
+console.log(myobj.prop2);
